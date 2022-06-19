d2c656852651c1839b2adbf77f1d0b575865c1ee
cuadrosignacio <ignacio.cuadros@anjanadata.com>
2022-06-19 20:54:32 +0200

    name centered position absolute

---
+++ index.js
@@ -0,0 +1,7 @@
+const ignacioSvg = document.querySelector('#ignacio');
+const cuadrosSvg = document.querySelector('#cuadros');
+const ignacioSvgPositionTop = ignacioSvg.getBoundingClientRect().top;
+const xUnit = ignacioSvg.height.baseVal.value / 2;
+
+// cuadrosSvg.style.top = (ignacioSvgPositionTop + xUnit * 3) + 'px';
+
