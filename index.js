837478e8ee7c68443bfff2e9427badaa05fed7c3
cuadrosignacio <ignacio.cuadros@anjanadata.com>
2022-07-04 19:10:19 +0200

    improve in the home and effect

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const ignacioSvg = document.querySelector('#ignacio');
-const cuadrosSvg = document.querySelector('#cuadros');
-const ignacioSvgPositionTop = ignacioSvg.getBoundingClientRect().top;
-const xUnit = ignacioSvg.height.baseVal.value / 2;
+const ignacioSvg = document.getElementById('ignacio');
+const cuadrosSvg = document.getElementById('cuadros');
 
-// cuadrosSvg.style.top = (ignacioSvgPositionTop + xUnit * 3) + 'px';
+window.addEventListener('scroll', () => {
+    let paddingAvailable = 22.5;
+    let toTranslate = (paddingAvailable * window.pageYOffset) / window.innerHeight;
 
+    if (toTranslate < paddingAvailable) {   
+        ignacioSvg.style.transform = `translateX(-${toTranslate}vw)`;
+        cuadrosSvg.style.transform = `translateX(${toTranslate}vw)`;
+    }
+    
+    const widthX = getComputedStyle(document.documentElement).getPropertyValue('--widthX');
+    if(widthX !== `${50}vw`) {
+        document.documentElement.style.setProperty('--widthX', `${parseInt(widthX) - 1}vw`);
+    }
+});
