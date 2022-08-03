10929b39dfff1aaac8749cf4a9473957d87ebe48
cuadrosignacio <ignacio.cuadros@anjanadata.com>
2022-08-03 22:03:18 +0200

    new home effects focus on mobile

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
 const ignacioSvg = document.getElementById('ignacio');
 const cuadrosSvg = document.getElementById('cuadros');
-let lastScroll = 0;
-const root = document.querySelector(':root');
-rootStyle = getComputedStyle(root);
+const svgInitial = ignacioSvg.getBoundingClientRect();
+
+ignacioSvg.style.top = `${(window.innerHeight / 2) - (parseInt(svgInitial.height) * 1.25)}px`;
+cuadrosSvg.style.bottom = `${(window.innerHeight / 2) - (parseInt(svgInitial.height) * 1.25)}px`;
+
+const svgInitialSet = ignacioSvg.getBoundingClientRect();
+
+let start, previousTimeStamp;
+let done = false;
+let scaleLimit = 0.5;
+let actualScale = 1;
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY > lastScroll) {
-        const cuadrosSvgInfo = cuadrosSvg.getBoundingClientRect();
-    
-        if ((cuadrosSvgInfo.top + cuadrosSvgInfo.height + 20) < window.innerHeight) {
-            cuadrosSvg.style.top = cuadrosSvgInfo.top + 7.5 + 'px';
-        }
-    
-        const ignacioSvgInfo = ignacioSvg.getBoundingClientRect();
-    
-        if ((ignacioSvgInfo.top > 20)) {
-            ignacioSvg.style.top = ignacioSvgInfo.top - 7.5 + 'px';
-        }
-    
-        if (window.scrollY > (window.innerHeight - 150)) {
-            document.querySelector('.presentation').classList.add('fade-out');
-        }
-
-        const actualHeight = parseInt(rootStyle.getPropertyValue('--heightX'));
-        if (actualHeight > 9) {
-            root.style.setProperty('--heightX', `${actualHeight - 1}vh`); 
-        }
-
-        lastScroll = window.scrollY;
-    }
+    window.requestAnimationFrame(step);
 });
+
+function step(timestamp) {
+    if (start === undefined) {
+      start = timestamp;
+    }
+
+    const elapsed = timestamp - start;
+  
+    if (previousTimeStamp !== timestamp) {
+        const limit = parseInt(svgInitialSet.top) - 20;
+        const count = Math.min(0.2 * elapsed, limit);
+
+        ignacioSvg.style.transform = `translateY(-${count}px)`;
+        ignacioSvg.style.transform += `scale(${actualScale})`;
+        cuadrosSvg.style.transform = `translateY(${count}px)`;
+        cuadrosSvg.style.transform += `scale(${actualScale})`;
+
+        if (actualScale > scaleLimit) {
+            actualScale -= 0.01;
+        }
+        if (count === limit) done = true;
+    }
+  
+    if (elapsed < 3000) { // Stop the animation after 3 seconds
+      previousTimeStamp = timestamp;
+      if (!done) {
+        window.requestAnimationFrame(step);
+      }
+    }
+  }
